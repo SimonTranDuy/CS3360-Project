eaf28c1e9add2a636ab9ab2dba463447ae4ce109
SimonTranDuy <duycucai2014@gmail.com>
2024-05-02 16:27:44 +0700

    CREATE BACK-END PROJECT

---
+++ backendcs3360/src/main/java/backend/com/example/backendcs3360/Backendcs3360Application.java
@@ -0,0 +1,13 @@
+package backend.com.example.backendcs3360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backendcs3360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Backendcs3360Application.class, args);
+	}
+
+}
