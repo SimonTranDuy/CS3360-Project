bcef51a2d549a103fa22e4025b0204bb042c422d
TGaDev <hoangthuychimte@gmail.com>
2024-05-04 10:36:18 +0700

    Add All Files

--- backendcs3360/src/main/java/backend/com/example/backendcs3360/Backendcs3360Application.java
+++ backendcs3360/src/main/java/backend/com/example/backendcs3360/Backendcs3360Application.java
@@ -1,14 +1,19 @@
 package backend.com.example.backendcs3360;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.boot.SpringApplication;
+// import backend.com.example.backendcs3360.Models.Customer;
 
 @SpringBootApplication
 public class Backendcs3360Application {
 
 	public static void main(String[] args) {
 		// Hello World!
-		SpringApplication.run(Backendcs3360Application.class, args);
+		// Test toString()
+		// SpringApplication.run(Backendcs3360Application.class, args);
+		// Customer customer1 = new Customer(123, "Tuan Anh", "0123456789");
+		// Customer customer2 = new Customer(456, "Khanh Duy", "0987654321");
+		// String newString = customer1.toString();
+		// System.out.println(newString);
 	}
-
 }
