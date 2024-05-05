cf809ebc041a7d5c4a41940a965045a97cf18cc9
SimonTranDuy <duycucai2014@gmail.com>
2024-05-06 00:11:30 +0700

    modify 3

--- backendcs3360/src/main/java/backend/com/example/backendcs3360/Repositories/CartReporitory.java
+++
@@ -1,9 +0,0 @@
-package backend.com.example.backendcs3360.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import backend.com.example.backendcs3360.models.Cart;
-
-public interface CartReporitory extends JpaRepository<Cart, Integer> {
-    
-}
--- backendcs3360/src/main/java/backend/com/example/backendcs3360/repositories/ClothesRepository.java
+++
@@ -1,9 +0,0 @@
-package backend.com.example.backendcs3360.repositories;
-
-import backend.com.example.backendcs3360.models.Clothes;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClothesRepository extends JpaRepository<Clothes, Integer> {
-
-}
