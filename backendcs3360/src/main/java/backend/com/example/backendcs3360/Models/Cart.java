95e762dd7bc2a6389e121c0a53eed9ea11de111c
SimonTranDuy <duycucai2014@gmail.com>
2024-05-06 00:16:04 +0700

    CREATE MAIN BRANCH

--- backendcs3360/src/main/java/backend/com/example/backendcs3360/Models/Cart.java
+++
@@ -1,8 +0,0 @@
-// package backend.com.example.backendcs3360.models;
-
-// import jakarta.persistence.*;
-
-// @Entity
-// public class Cart {
-    
-// }
--- backendcs3360/src/main/java/backend/com/example/backendcs3360/Models/Orders.java
+++
@@ -1,5 +0,0 @@
-// package backend.com.example.backendcs3360.models;
-
-// public class Orders {
-    
-// }
