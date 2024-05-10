8f4a59c318139270860adab44b50e89963cf946b
SimonTranDuy <duycucai2014@gmail.com>
2024-05-11 02:16:40 +0700

    optimize OrderItem code

--- backendcs3360/src/main/java/backend/com/example/backendcs3360/repositories/OrderItemRepository.java
+++ backendcs3360/src/main/java/backend/com/example/backendcs3360/repositories/OrderItemRepository.java
@@ -3,11 +3,9 @@ package backend.com.example.backendcs3360.repositories;
 import backend.com.example.backendcs3360.dto.OrderItemDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Date;
 import java.util.List;
 
 public interface OrderItemRepository extends JpaRepository<OrderItemDTO, Integer> {
-    List<OrderItemDTO> findByCustomerIdAndDateOfPurchase(int customerId, Date dateOfPurchase);
     List<OrderItemDTO> findByCustomerIdAndDateOfPurchaseIsNull(int customerId);
     List<OrderItemDTO> findByCustomerIdAndDateOfPurchaseIsNotNullOrderByDateOfPurchaseDesc(int customerId);
     List<OrderItemDTO> findByCustomerIdAndDateOfPurchaseIsNotNullOrderByDateOfPurchaseAsc(int customerId);
