d5882ecd7ef0ff58b30dcd0213f4b8fd409f0a9b
ndtctk1295 <nguyentuananh190403@gmail.com>
2024-05-15 09:50:33 +0700

    update 12-05-2024 testing ordersitem oke, need to adjust some functions

--- backendcs3360/src/main/java/backend/com/example/backendcs3360/repositories/CustomerRepository.java
+++ backendcs3360/src/main/java/backend/com/example/backendcs3360/repositories/CustomerRepository.java
@@ -8,4 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<CustomerDTO, Integer> {
     Optional<CustomerDTO> findByPhoneNumber(String phoneNumber);
+    Optional<CustomerDTO> findById (int customerId);
 }
