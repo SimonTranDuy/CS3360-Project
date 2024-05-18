9045cc6d95f8b19120c7ac365435c16f4ab21b06
ndtctk1295 <nguyentuananh190403@gmail.com>
2024-05-18 23:18:52 +0700

    final code before test 18-05-2024

--- frontendcs3360/js/renderHistory.js
+++ frontendcs3360/js/renderHistory.js
@@ -8,10 +8,10 @@ async function fetchOrderHistory(order = "desc") {
       ? `http://localhost:8080/api/orderItems/historyAsc/${customerId}`
       : `http://localhost:8080/api/orderItems/historyDesc/${customerId}`;
   console.log(url);
-  const testURL = `http://localhost:8080/api/orderItems/historyAsc/10`;
+  const testURL = `http://localhost:8080/api/orderItems/historyAsc/1`;
   console.log(customerInfo);
   try {
-    const response = await fetch(url, {
+    const response = await fetch(testURL, {
       method: "GET",
       mode: "cors", // Use "cors" instead of "no-cors"
       headers: {
@@ -31,43 +31,30 @@ async function fetchOrderHistory(order = "desc") {
   }
 }
 
-// Gọi hàm fetchOrderHistory với 'desc' khi trang được tải
-document.addEventListener("DOMContentLoaded", function () {
-  fetchOrderHistory();
-  renderOrderHistory();
-});
-
 // Thêm sự kiện click cho các nút sắp xếp và làm mới
-document
-  .getElementById("sort-desc")
-  .addEventListener("click", () => fetchOrderHistory("desc"));
-document
-  .getElementById("sort-asc")
-  .addEventListener("click", () => fetchOrderHistory("asc"));
-document
-  .getElementById("refresh-orders")
-  .addEventListener("click", () => fetchOrderHistory());
-
 const renderOrderHistory = () => {
   const tbody = document.getElementById("order-history-body");
   tbody.innerHTML = ""; // Xóa nội dung cũ
   const data = JSON.parse(localStorage.getItem("orderHistoryData"));
+
   data.orderItems.forEach((orderItem) => {
     const row = document.createElement("tr");
     row.innerHTML = `
-              <td class="px-4 py-2">${orderItem.listOfItemsId}</td>
+    <td class="px-4 py-2">${orderItem.item.productName}</td>
+    <td class="px-4 py-2">${orderItem.quantity}</td>
+    <td class="px-4 py-2">${orderItem.item.price}</td>
+    <td class="px-4 py-2">${orderItem.item.price * orderItem.quantity}</td>
               <td class="px-4 py-2">${new Date(
                 orderItem.dateOfPurchase
               ).toLocaleDateString()}</td>
-              <td class="px-4 py-2">${orderItem.orderCode}</td>
-              <td class="px-4 py-2">${orderItem.customer.phoneNumber}</td>
-              <td class="px-4 py-2">${orderItem.customer.address}</td>
-              <td class="px-4 py-2">${orderItem.item.productName}</td>
-              <td class="px-4 py-2">${orderItem.quantity}</td>
-              <td class="px-4 py-2">
-                <button class="btn">See Details</button>
-              </td>
+
             `;
     tbody.appendChild(row);
   });
 };
+
+// Gọi hàm fetchOrderHistory với 'desc' khi trang được tải
+document.addEventListener("DOMContentLoaded", async function () {
+  await fetchOrderHistory();
+  renderOrderHistory();
+});
